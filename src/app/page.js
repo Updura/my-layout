60a44b653f10da4f2ef5d8bb47eb0387576c30f3
Updura <hackerhat9098@gmail.com>
2024-06-25 15:01:29 +0700

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,37 @@
+export default function Home() {
+  return (
+    <>
+<div class="container-fluid">
+  <div class="row text-center">
+    <div class="col-md-12 bg-success p-5">
+      Column1
+    </div>
+
+    <div class="col-md-12 bg-danger p-5">
+      Column2
+    </div>
+
+    <div class="col-md-12 bg-warning p-5">
+      Column3
+    </div>
+
+   <div className="row text-center"></div>
+
+    <div class="col-md-4 bg-success p-5">
+      Column1
+    </div>
+    <div class="col-md-4 bg-primary p-5">
+      Column1
+    </div>
+    <div class="col-md-4 bg-secondary p-5">
+      Column1
+    </div>
+
+    <div class="col-md-12 bg-info p-5">
+      Columnn4
+    </div>
+  </div>
+</div>
+    </>
+  );
+}
